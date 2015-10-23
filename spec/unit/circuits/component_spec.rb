d9ae751135f8ada0aac75242c72ec2916d5d0d8c
dev-henryMP <henry.murupaenga@xero.com>
2015-10-23 18:48:10 +1300

    restored regessed tests

--- spec/unit/circuits/component_spec.rb
+++ spec/unit/circuits/component_spec.rb
@@ -8,14 +8,40 @@ class MockComponent
   def initialize(opts = {})
     @outputs = opts[:outputs]
   end
+
+  def mock_input_count
+    input_count
+  end
+
+  def mock_output_count
+    output_count
+  end
 end
 
 describe Circuits::Component do
+  let(:outputs) { [double('output')] }
+  
+  subject { MockComponent.new(outputs: outputs) }
+
+  describe '#input_count' do
+    it 'is not implemented' do
+      expect { subject.mock_input_count }.to raise_error(NotImplementedError)
+    end
+  end
+
+  describe '#output_count' do
+    it 'is not implemented' do
+      expect { subject.mock_output_count }.to raise_error(NotImplementedError)
+    end
+  end
+
+  describe '#tick' do
+    it 'is not implemented' do
+      expect { subject.tick }.to raise_error(NotImplementedError)
+    end
+  end
+
   describe '#tock' do
-    let(:outputs) { [double('output')] }
-
-    subject { MockComponent.new(outputs: outputs) }
-
     it 'tocks the outputs' do
       expect(outputs[0]).to receive(:tock)
       subject.tock
