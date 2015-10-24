3eb479e41b82ac926b5d470db2785b982478b105
Kierran McPherson <kierranm@gmail.com>
2015-10-25 11:02:35 +1300

    Add specs to test components with extra inputs

--- spec/unit/circuits/component/and_spec.rb
+++ spec/unit/circuits/component/and_spec.rb
@@ -55,5 +55,53 @@ describe Circuits::Component::And do
         end
       end
     end
+
+    [3, 4, 8].each do |n|
+      context "with #{n} inputs" do
+        subject { Circuits::Component::And.new inputs: n }
+
+        before do
+          n.times { |x| subject.inputs[x].set inputs[x] }
+        end
+
+        context 'when all inputs are true' do
+          let(:inputs) { n.times.collect { true } }
+
+          it '= true' do
+            subject.tick
+            subject.tock
+            expect(subject.outputs[0].get).to eq(true)
+          end
+        end
+
+        context 'when all inputs are false' do
+          let(:inputs) { n.times.collect { false } }
+
+          it '= false' do
+            subject.tick
+            subject.tock
+            expect(subject.outputs[0].get).to eq(false)
+          end
+        end
+
+        context 'when any input is false' do
+          n.times do |x|
+            context "when input #{x} is false" do
+              let(:inputs) do
+                inputs =  n.times.collect { true }
+                inputs[x] = false
+                inputs
+              end
+
+              it '= false' do
+                subject.tick
+                subject.tock
+                expect(subject.outputs[0].get).to eq(false)
+              end
+            end
+          end
+        end
+      end
+    end
   end
 end
