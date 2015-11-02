b83955c1cfef4308031c5244dd8fbdb6b8ec465f
dev-henryMP <henry.murupaenga@xero.com>
2015-11-03 10:37:46 +1300

    refactor adder in terms of half adder

--- lib/circuits/component/full_adder.rb
+++ lib/circuits/component/full_adder.rb
@@ -1,7 +1,6 @@
 require 'circuits/component/base'
-require 'circuits/component/and'
+require 'circuits/component/half_adder'
 require 'circuits/component/or'
-require 'circuits/component/xor'
 
 module Circuits
   module Component
@@ -20,50 +19,36 @@ module Circuits
 
       def create_sub_components
         {
-          and_in: And.new,
-          and_carry: And.new,
+          half_adder_in: HalfAdder.new,
+          half_adder_carry: HalfAdder.new,
           or_gate: Or.new,
-          xor_in: Xor.new,
-          xor_out: Xor.new
         }
       end
 
       def link(sub_components)
-        link_and_in sub_components
-        link_and_carry sub_components
+        link_half_adder_in sub_components
+        link_half_adder_carry sub_components
         link_or_gate sub_components
-        link_xor_in sub_components
-        link_xor_out sub_components
         link_outputs sub_components
       end
 
-      def link_and_in(sub_components)
-        sub_components[:and_in].a.set a
-        sub_components[:and_in].b.set b
+      def link_half_adder_in(sub_components)
+        sub_components[:half_adder_in].a.set a
+        sub_components[:half_adder_in].b.set b
       end
 
-      def link_and_carry(sub_components)
-        sub_components[:and_carry].a.set sub_components[:xor_in].out
-        sub_components[:and_carry].b.set c_in
+      def link_half_adder_carry(sub_components)
+        sub_components[:half_adder_carry].a.set sub_components[:half_adder_in].s
+        sub_components[:half_adder_carry].b.set c_in
       end
 
       def link_or_gate(sub_components)
-        sub_components[:or_gate].a.set sub_components[:and_in].out
-        sub_components[:or_gate].b.set sub_components[:and_carry].out
-      end
-
-      def link_xor_in(sub_components)
-        sub_components[:xor_in].a.set a
-        sub_components[:xor_in].b.set b
-      end
-
-      def link_xor_out(sub_components)
-        sub_components[:xor_out].a.set sub_components[:xor_in].out
-        sub_components[:xor_out].b.set c_in
+        sub_components[:or_gate].a.set sub_components[:half_adder_carry].c
+        sub_components[:or_gate].b.set sub_components[:half_adder_in].c
       end
 
       def link_outputs(sub_components)
-        s.set sub_components[:xor_out].out
+        s.set sub_components[:half_adder_carry].s
         c_out.set sub_components[:or_gate].out
       end
     end
